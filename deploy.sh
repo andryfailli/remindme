ba136b197aa221b5056e3454498579a5882fb78b
Andrea Failli <andry.failli@gmail.com>
2018-01-27 18:02:24 +0100

    secret from env

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ kubectl create -f ./remindme-notifier-autoscaler.yaml
 kubectl create -f ./remindme-notifier-cron.yaml
 kubectl create -f ./remindme-frontend-deployment.yaml
 kubectl create -f ./remindme-frontend-service.yaml
+sed -i "s/<public-cert-pem-file-base64-encoded>/$REMINDME_SECRET_CRT/g" remindme-secret.yaml
+sed -i "s/<private-key-file-base64-encoded>/$REMINDME_SECRET_KEY/g" remindme-secret.yaml
 kubectl create -f ./remindme-secret.yaml
 kubectl create -f ./remindme-ingress.yaml
