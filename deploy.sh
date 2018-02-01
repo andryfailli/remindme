f4e6f4f223a6cffa10ba2b138db255ed5cfe3fa2
Andrea Failli <andry.failli@gmail.com>
2018-02-01 23:42:15 +0100

    Release/v0.0.1 (fix) (#6)
    release v0.0.1

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ kubectl create -f ./remindme-notifier-cron.yaml
 kubectl create -f ./remindme-frontend-deployment.yaml
 kubectl create -f ./remindme-frontend-service.yaml
 kubectl create -f ./remindme-frontend-autoscaler.yaml
-sed -i "s/<public-cert-pem-file-base64-encoded>/$REMINDME_SECRET_CRT/g" remindme-secret.yaml
-sed -i "s/<private-key-file-base64-encoded>/$REMINDME_SECRET_KEY/g" remindme-secret.yaml
+sed -i "s/<public-cert-pem-file-base64-encoded>/$REMINDME_SECRET_CRT/g" ./remindme-secret.yaml
+sed -i "s/<private-key-file-base64-encoded>/$REMINDME_SECRET_KEY/g" ./remindme-secret.yaml
 kubectl create -f ./remindme-secret.yaml
 kubectl create -f ./remindme-ingress.yaml
